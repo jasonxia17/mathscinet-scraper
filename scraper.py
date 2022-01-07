4c369cb0f74d4cb691a9fde14af2fa0041d30ee2
jasonxia17 <jasonx3@illinois.edu>
2022-01-07 13:56:16 -0600

    Use pandas to write to a csv

--- scraper.py
+++ scraper.py
@@ -1,11 +1,13 @@
-from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+import pandas as pd
 
 MATHSCINET_AUTHOR_SEARCH_URL = "https://mathscinet-ams-org.proxy2.library.illinois.edu/mathscinet/searchauthors.html"
 
@@ -16,44 +18,42 @@ driver = webdriver.Chrome(service=service)
 wait = WebDriverWait(driver, 60)
 
 with open("names.txt", "r") as f:
-    names = f.read().splitlines()
+    names = f.read().splitlines()[:3]
 
-with open("output.txt", "w") as outfile:
-    for name in names:
-        driver.get(MATHSCINET_AUTHOR_SEARCH_URL)
+df = []
 
-        # wait up to 60 seconds for the user to log in
-        author_name_input_field = wait.until(EC.visibility_of_element_located((By.NAME, "authorName")))
+for name in names:
+    driver.get(MATHSCINET_AUTHOR_SEARCH_URL)
 
-        author_name_input_field.send_keys(name)
-        author_name_input_field.send_keys(Keys.ENTER)
+    # wait up to 60 seconds for the user to log in
+    author_name_input_field = wait.until(EC.visibility_of_element_located((By.NAME, "authorName")))
 
-        print("==================================", file=outfile)
-        print(name, file=outfile)
+    author_name_input_field.send_keys(name)
+    author_name_input_field.send_keys(Keys.ENTER)
 
-        match_divs = driver.find_elements(By.CLASS_NAME, "matches")
-        if match_divs:
-            # there were no matches or multiple matches for the name, so we're still on the search results page
-            # instead of the profile page of a specific author
-            if match_divs[0].text == "Matches: 0":
-                print("No matches were found for this name on MathSciNet", file=outfile)
-            else:
-                # TODO: is there a way to get university affiliation from MathSciNet to filter out some matches?
-                print("Multiple matches were found for this name on MathSciNet", file=outfile)
-            continue
+    if driver.find_elements(By.CLASS_NAME, "matches"):
+        # there were no matches or multiple matches for the name, so we're still on the search results page
+        # instead of the profile page of a specific author
+        # just append the name and leave the rest of the columns blank
+        df.append([name])
+        continue
 
-        table_cells = driver.find_elements(By.TAG_NAME, "td")
-        total_citations = None
-        total_publications = None
+    table_cells = driver.find_elements(By.TAG_NAME, "td")
+    total_citations = None
+    total_publications = None
 
-        # TODO: analyze table cells by <tr> (table rows) instead?
-        for prev_cell, curr_cell in zip(table_cells[:-1], table_cells[1:]):
-            if prev_cell.text == "Total Publications:":
-                total_publications = wait.until(lambda driver: curr_cell.text)
-            elif prev_cell.text == "Total Citations:":
-                total_citations = wait.until(lambda driver: curr_cell.text)
-                # need to wait a bit for the text to show up in the table cells
-                # mathscinet probably needs some time to get the data from a database
+    for prev_cell, curr_cell in zip(table_cells[:-1], table_cells[1:]):
+        if prev_cell.text == "Total Publications:":
+            total_publications = wait.until(lambda driver: curr_cell.text)
+        elif prev_cell.text == "Total Citations:":
+            total_citations = wait.until(lambda driver: curr_cell.text)
+            # need to wait a bit for the text to show up in the table cells
+            # mathscinet probably needs some time to get the data from a database
 
-        print("Total Publications:", total_publications, file=outfile)
-        print("Total Citations:", total_citations, file=outfile)
+    df.append([name, int(total_publications), int(total_citations)])
+
+df = pd.DataFrame(df, columns=["Name", "Total Publications", "Total Citations"])
+df.set_index("Name", inplace=True)
+df.loc["Total"] = df.sum()
+
+df.to_csv("output.csv")
